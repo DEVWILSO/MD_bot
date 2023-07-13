6b25a71884fa79e81bb19137dcf55fb5ece3d114
DevWilson <106603032+DEVWILSO@users.noreply.github.com>
2023-07-13 21:02:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repository"
-git clone https://github.com/DEVWILSO/MD_bot
+git clone https://github.com/DEVWILSO/MD_bot /md-renamebot
 cd /md-renamebot 
 echo "installing requirements"
 pip3 install -U -r requirements.txt
